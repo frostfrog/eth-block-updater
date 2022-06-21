e0d594ee8c902fb40e9e8cacac81e4a5b5ad3570
frostfrog <frostfrog2@gmail.com>
2022-06-21 11:22:01 +0800

    Implement the block updater

--- contracts/BlockUpdater.sol
+++ contracts/BlockUpdater.sol
@@ -1,6 +1,6 @@
+// SPDX-License-Identifier: MIT
 
-
-
+pragma solidity ^0.8.14;
 
 abstract contract BlockUpdater {
     function verifyBlocks(bytes32[] calldata blockhashes, bytes32 proof) internal virtual;
@@ -8,7 +8,38 @@ abstract contract BlockUpdater {
     function updateBlock(bytes32[] calldata blockhashes, bytes32 proof, uint256[] calldata totalDifficulty) public {
         verifyBlocks(blockhashes, proof);
 
-        // handling the canonical 
+        // make sure the known block
+        bytes32 parent = blockhashes[0];
+        BlockInfo storage parentInfo = blocks[parent];
+        BlockInfo memory tempInfo;
+
+        require(parent == canonical[0] || parentInfo.blockNumber != 0, 'Cannot find parent');
+
+        for (uint i = 1; i < blockhashes.length; i++) {    
+            tempInfo.blockNumber = i + parentInfo.blockNumber;
+            tempInfo.totalDifficulty = totalDifficulty[i];
+
+            blocks[blockhashes[i]] = tempInfo;
+        }
+        bytes32 h = canonical[canonical.length - 1];
+
+        if (blocks[h].totalDifficulty < totalDifficulty[totalDifficulty.length - 1]) {
+            // reorg
+            for (uint i = 1; i < blockhashes.length; i++) {    
+                uint number = i + parentInfo.blockNumber;
+                if (number >= canonical.length) {
+                    canonical.push(blockhashes[i]);
+                } else {
+                    canonical[number] = blockhashes[i];
+                }
+            }
+        }
+    }
+
+    function head() public view returns(uint256 blockNumber, bytes32 the_hash, uint256 totalDifficulty) {
+        blockNumber = canonical.length;
+        the_hash = canonical[blockNumber];
+        totalDifficulty = blocks[the_hash].totalDifficulty;
     }
 
     struct BlockInfo {
